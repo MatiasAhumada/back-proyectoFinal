3fac6aac98c2ac54b912e61ff355ced8e74a5c9b
Alvaro Ruiz Mejia <zhuone@hotmail.com>
2022-11-09 19:36:34 -0300

    agregue middlewares, carpetas y archivos controllers, models y routes basicas y un archivo statico

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import express from "express";
+import morgan from "morgan";
+import cors from "cors";
 
 const app = express();
 
@@ -7,3 +9,9 @@ app.set("port", process.env.PORT || 4000)
 app.listen(app.get("port"), ()=>{
     console.log("Puerto " + app.get("port"))
 })
+
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, "../public")))
