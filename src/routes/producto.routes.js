ae226d27cdb437b87f041e813ca2dac627f0de5b
matias <pagosptcmatias@gmail.com>
2022-11-10 18:46:00 -0300

    linkee el router con el index

--- src/routes/producto.routes.js
+++ src/routes/producto.routes.js
@@ -1,19 +1,11 @@
 import { Router } from "express";
-import {
- 
-  
-  editarProducto,
- 
-} from "../controllers/productos.controllers";
+import { editarProducto } from "../controllers/productos.controllers";
 
 const router = Router();
 
-
-
 router
   .route("/productos/:id")
-  
-  .put(editarProducto)
- 
+
+  .put(editarProducto);
 
 export default router;
